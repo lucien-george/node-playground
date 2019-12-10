4dbd381a1932eb1b7968544210f6e3bb1fa5d79d
Lucien George <luciengeorge95@gmail.com>
2019-12-10 16:48:50 +0000

    lecture 79 - promises

--- callback.js
+++ callback.js
@@ -1,9 +1,14 @@
-const add = (a, b, callback) => {
+const doWorkCallback = (callback) => {
   setTimeout(() => {
-    callback(a + b);
-  }, 2000);
-}
+    // callback('this is my error', undefined);
+    callback(undefined, [1, 4, 7]);
+  }, 2000)
+};
 
-add(1, 4, (sum) => {
-  console.log(sum);
+doWorkCallback((error, result) => {
+  if (error) {
+    return console.log(error);
+  }
+
+  console.log(result)
 });
