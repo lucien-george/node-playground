e36dc550141ff7c31375e85a7a2afa8399fa1c53
Lucien George <luciengeorge95@gmail.com>
2020-02-17 15:41:08 +0000

    promise chaining

--- promises.js
+++ promises.js
@@ -1,20 +1,49 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve([1, 4, 7]);
-    reject('Things went wrong');
-  }, 2000)
-});
+// const doWorkPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve([1, 4, 7]);
+//     reject('Things went wrong');
+//   }, 2000)
+// });
 
-doWorkPromise.then((result) => {
-  console.log('Success!', result);
-}).catch((error) => {
-  console.log('Error!', error);
+// doWorkPromise.then((result) => {
+//   console.log('Success!', result);
+// }).catch((error) => {
+//   console.log('Error!', error);
+// })
+
+// //
+// //                              fullfilled (resolve)
+// //                            /
+// // Promise    -- pending -->
+// //                            \
+// //                              rejected (reject)
+// //
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  })
+};
+
+// add(1, 2).then((sum) => {
+//   console.log(sum);
+//   add(sum, 5).then((sum2) => {
+//     console.log(sum2);
+//   }).catch((e) => {
+//     console.error(e);
+//   })
+// }).catch((e) => {
+//   console.error(e);
+// });
+
+// promise chaining
+add(1, 1).then((sum) => {
+  console.log(sum)
+  return add(sum, 4);
+}).then((sum2) => {
+  console.log(sum2);
+}).catch((e) => {
+  console.error(e);
 })
-
-//
-//                              fullfilled (resolve)
-//                            /
-// Promise    -- pending -->
-//                            \
-//                              rejected (reject)
-//
